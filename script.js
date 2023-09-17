75280f3f7e30127a65a9bf93ccb7e82e8815d5c4
RafatSh-hash <rafatsh800@gmail.com>
2023-09-17 13:43:35 +0600

    restarted

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
+let animationStared = false;
+const startButton = document.getElementById("startButton");
 const scoreCard = document.getElementById('scoreCard');
 let score = 0;
 
-
-for (let i = 1; i <= 10; i++) {
-    const btn = document.getElementById(`btn${i}`);
-    
-    // Add a click event listener to each button
-    btn.addEventListener('click', function () {
-        // Change the background color to red
-        btn.style.backgroundColor = 'red';
-        score += 1;
-        scoreCard.innerHTML = score;
-        btn.innerHTML = "🫠";
-
-    });
-}
+startButton.addEventListener("click", () =>{
+    animationStared = true;
+    const btn = document.getElementsByClassName(".btn");
+    if(animationStared){
+        
+        btn.classList.add("x");
+    }
+    else{
+     
+        btn.classList.remove("x");
+    }
+})

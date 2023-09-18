ff888f6ee8a9792c71a3b2d917f197f75079509c
RafatSh-hash <rafatsh800@gmail.com>
2023-09-18 10:49:24 +0600

    Added start-stop button functionality

--- script.js
+++ script.js
@@ -1,17 +1,56 @@
-let animationStared = false;
+
+let animationStarted = false;
 const startButton = document.getElementById("startButton");
+const btns = document.querySelectorAll(".btn");
 const scoreCard = document.getElementById('scoreCard');
 let score = 0;
 
-startButton.addEventListener("click", () =>{
-    animationStared = true;
-    const btn = document.getElementsByClassName(".btn");
-    if(animationStared){
+
+startButton.addEventListener("click", () => {
+    animationStarted = !animationStarted;
+    
+    if (animationStarted) {
         
-        btn.classList.add("x");
+        btns.forEach(btn => {
+            btn.classList.remove("x");
+        });
+        
+        startButton.textContent = "Start"; 
+        addClickEventListeners();
+
+    } else {
+       
+        btns.forEach(btn => {
+            btn.classList.add("x");
+        });
+        startButton.textContent = "Stop";
+        removeEventListener() // Change button text to "Stop"
     }
-    else{
-     
-        btn.classList.remove("x");
+
+
+});
+
+
+function addClickEventListeners(){
+    for(let i = 1; i <= 15; i++) {
+
+        const smileyButton = document.getElementById(`btn${i}`)
+    
+        smileyButton.addEventListener("click", () => {
+            console.log("Hello")
+             // Check if animation is playing
+                smileyButton.style.backgroundColor = "red";
+                smileyButton.innerText = "🫠";
+                score++;
+                scoreCard.textContent = score;
+            
+        })
+    
     }
-})
+}
+
+function removeEventListener(){
+
+
+
+}
